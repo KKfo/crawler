a172698a4901f4cebc070138bf695b30f1ba7b3f
koolkat <flores_a@epitech.eu>
2015-11-03 22:30:42 +0100

    Refactori,g with logs etc

--- app/crawler.py
+++ app/crawler.py
@@ -13,15 +13,16 @@ import re
 import codecs
 import math
 import time
+import math
+import logging
 
 if (len(sys.argv) != 2):
     print('Usage:'+sys.argv[0]+' http://example.com')
 
-SEED = sys.argv[1]
+seed = sys.argv[1]
 VIS = set()
 ADD = lambda u: VIS.add(u) or u
-URL = sys.argv[1]
-IS_LOCAL_DOMAIN = lambda u: urlparse(URL).netloc == urlparse(u).netloc
+IS_LOCAL_DOMAIN = lambda u: urlparse(seed).netloc == urlparse(u).netloc
 NOT_MEDIA = lambda u: not re.compile(r'^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png|js|css|less|sass)$').match(u)
 NOT_INDEXED = lambda u: u not in VIS
 REQUESTS = 0
@@ -31,27 +32,37 @@ def print_info():
     global START_T
     global REQUESTS
     REQUESTS += 1
-    print("Pages visited:c", REQUESTS)
-    print("Requests per second: ",REQUESTS/(time.time()-START_T))
+    print("Time elapsed: ", math.floor((time.time()-START_T)))
     print("Links saved: ",len(VIS))
+    print("Pages visited:c", REQUESTS)
+    print("Requests/second: ",REQUESTS//(time.time()-START_T))
 
-async def get(u,c):
-    async with c.get(u) as response:
-        page = await response.read()
-    print_info()
-    soup = BeautifulSoup(page,'lxml').find_all('a')
-    links  = [
-        ADD(urljoin(URL, link.get('href'))) for link in soup  if 
-        NOT_MEDIA(urljoin(URL, link.get('href'))) and
-        IS_LOCAL_DOMAIN(urljoin(URL, link.get('href'))) and
-        NOT_INDEXED(urljoin(URL, link.get('href')))]
-    if (links):
-        asyncio.wait([asyncio.ensure_future(get(li, c)) for li in links])
+def is_ok(link):
+    url = urljoin(seed, link)
+    return NOT_MEDIA(url) and \
+        IS_LOCAL_DOMAIN(url) and \
+        NOT_INDEXED(url)
+
+async def get(u,c, log):
+    try:
+        async with c.get(u) as response:
+            page = await response.read()
+        print_info()
+        log("Visited: %s", u)
+        soup = BeautifulSoup(page,'lxml').find_all('a')
+        links  = [ADD(urljoin(seed, a.get('href')))
+                  for a in soup if is_ok(a.get('href'))]
+        if (links):
+            asyncio.wait([asyncio.ensure_future(get(li, c, log)) for li in links])
+    except Exception as e:
+        log("error %s reason: %s" % (u, e))
 
 def main():
+    log = logging.getLogger().info
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
     loop = asyncio.get_event_loop()
     client = aiohttp.ClientSession(loop=loop)
-    asyncio.wait(asyncio.ensure_future(get(SEED, client)))
+    asyncio.wait(asyncio.ensure_future(get(seed, client, log)))
     loop.run_forever() # Right now it will never return from here.
     client.close()
 
