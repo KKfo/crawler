9bb2cf0e99fdcc767a76c5b206da52cfdbbfb0c1
koolkat <flores_a@epitech.eu>
2015-11-03 05:35:49 +0100

    Refactor: add a loop in the coroutine to create less tasks

--- app/crawler.py
+++ app/crawler.py
@@ -15,30 +15,28 @@ URL = sys.argv[1]
 IS_LOCAL_DOMAIN = lambda u: urlparse(URL).netloc == urlparse(u).netloc
 NOT_MEDIA = lambda u: not re.compile(r'^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png|js|css|less|sass)$').match(u)
 NOT_INDEXED = lambda u: u not in VIS
-queue = []
 
-async def get(url):
-    print('sent request: '+url)
-    global queue
-    async with aiohttp.get(url) as response:
-        html = await response.read()
-    s = BeautifulSoup(html, 'lxml')
-    links = [
-        ADD(urljoin(URL, link.get('href'))) for link in s.find_all('a') if 
+async def get(urls):
+    print('sent request:')
+    pages = []
+    for u in urls:
+        async with aiohttp.get(u) as response:
+            pages.append(await response.read())
+    soups = [BeautifulSoup(page, 'lxml').find_all('a') for page in pages]
+    links  = [
+        ADD(urljoin(URL, link.get('href'))) for SOUP in soups for link in SOUP  if 
         NOT_MEDIA(urljoin(URL, link.get('href'))) and
         IS_LOCAL_DOMAIN(urljoin(URL, link.get('href'))) and
         NOT_INDEXED(urljoin(URL, link.get('href')))]
     print(len(VIS))
     if (links):
-        queue += [asyncio.ensure_future(get(link)) for link in links if link]
-        if (len(queue) > 500):
-            asyncio.wait(queue)
-            queue = []
-    
+        asyncio.wait(asyncio.ensure_future(get(links)))
+
 def main():
     loop = asyncio.get_event_loop()
-    asyncio.wait([asyncio.ensure_future(get(link)) for link in links])
+    asyncio.wait(asyncio.ensure_future(get(links)))
     loop.run_forever()
 
+# aiohttp.errors.ClientOSError:
 if __name__ == '__main__':
     main()
